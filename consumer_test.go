3d76901b3219c3a245c3167d1cd762c36ad3e019
Jason Coene <jcoene@gmail.com>
2016-03-12 12:04:54 -0600

    Add test

--- consumer_test.go
+++ consumer_test.go
@@ -1,6 +1,7 @@
 package que
 
 import (
+	"os"
 	"testing"
 )
 
@@ -20,5 +21,24 @@ func TestNewConsumer(t *testing.T) {
 
 	if c.Channel != "cha" {
 		t.Errorf("expected channel %s, got %s", "cha", c.Channel)
+	}
+}
+
+func TestGetConcurrency(t *testing.T) {
+	// Not set returns default
+	if v := getConcurrency("unknown_thing", 5); v != 5 {
+		t.Errorf("expected default to be 5, got %d", v)
+	}
+
+	// Set and valid returns value
+	os.Setenv("TOPIC_CONCURRENCY_USER_PROFILE", "50")
+	if v := getConcurrency("user_profile", 5); v != 50 {
+		t.Errorf("expected valid to be 50, got %d", v)
+	}
+
+	// Set and invalid returns default
+	os.Setenv("TOPIC_CONCURRENCY_USER_PROFILE", "doodads")
+	if v := getConcurrency("user_profile", 5); v != 5 {
+		t.Errorf("expected invalid to default to 5, got %d", v)
 	}
 }
